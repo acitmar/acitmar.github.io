8a50b5d9af6e47dfc32fd8f7d88befbb09a812c8
Vanessa Cruz <vanessa@yapo.cl>
2024-04-09 23:49:30 -0400

    cambios para pagina web logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { MemoryRouter, Switch, Route } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 
 import NavBarAcitmar from './components/navbar/NavBarAcitmar';
 import GroupCards from './components/GroupCards/GroupCards';
 import CarouselAcitmar from './components/CarouselAcitmar/CarouselAcitmar'
 import FooterAcitmar from './components/FooterAcitmar/FooterAcitmar';
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
 import logo from './img/acitmarLOGO.gif'
 import video from './img/videoAcitmar.gif';
-import visual_one from './img/visual1.gif'
-import visual_two from './img/visual2.gif'
+import visual_one from './img/visual1.gif';
+import visual_two from './img/visual2.gif';
 import BlogAcitmar from './components/BlogAcitmar/BlogAcitmar';
 
 import './App.css';
